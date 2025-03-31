fb8fe09c5a6c0aca942431b0487a7a08e663dfc0
sivasankar55 <sivasankar8875@gmil.com>
2025-03-31 22:13:46 +0530

    server error

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ import cookieParser from "cookie-parser";
 
  const PORT = process.env.PORT || 5000;
 
- app.use(express.json()); // allows you to parse the body of the request
+ app.use(express.json({limit:"10mb"})); // allows you to parse the body of the request
 app.use(cookieParser());
 
  app.use("/api/auth", authRoutes);
