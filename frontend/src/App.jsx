d61ab6fd35408d54e12f2673c2ca8c84e581ab4b
sivasankar55 <sivasankar8875@gmil.com>
2025-04-09 17:58:46 +0530

    featiredproducts function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import {Navigate, Route,Routes} from "react-router-dom";
-
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
@@ -61,4 +60,4 @@ const {getCartItems}= useCartStore();
   );
 }
 
-export default App
+export default App;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
-
+import { useEffect } from "react";
 import CategoryItem from "../components/CategoryItem";
-
+import { useProductStore } from "../stores/useProductStore";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 
 const categories = [
@@ -14,6 +15,12 @@ const categories = [
 ];
 
 const HomePage = () => {
+	const {fetchFeaturedProducts, products, isLoading} = useProductStore();
+
+	useEffect(() => {
+		fetchFeaturedProducts();
+	},[fetchFeaturedProducts]);
+
    return (
     <div className='relative min-h-screen text-white overflow-hidden'>
 			<div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
@@ -29,9 +36,10 @@ const HomePage = () => {
 						<CategoryItem category={category} key={category.name} />
 					))}
 				</div>
+				{!isLoading && products.length > 0 && <FeaturedProducts featuredProducts ={products}/>}
 			</div>
 		</div>
-   )
+   );
  };
  
- export default HomePage
+ export default HomePage;
