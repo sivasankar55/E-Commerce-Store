9154e33e9a38cb3db16b8e45d0ac82bfd42c8af0
sivasankar55 <sivasankar8875@gmil.com>
2025-04-09 20:59:22 +0530

    error check

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
  import express from "express";
  import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
  import authRoutes from "./routes/auth.route.js";
  import productRoutes from "./routes/product.route.js";
@@ -17,6 +18,8 @@ import cookieParser from "cookie-parser";
 
  const PORT = process.env.PORT || 5000;
 
+ const __dirname = path.resolve();
+
  app.use(express.json({limit:"10mb"})); // allows you to parse the body of the request
 app.use(cookieParser());
 
@@ -26,6 +29,14 @@ app.use(cookieParser());
  app.use("/api/coupons", couponsRoutes);
  app.use("/api/payments", paymentRoutes);
  app.use("/api/analytics", analyticsRoutes);
+
+ if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
  app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
